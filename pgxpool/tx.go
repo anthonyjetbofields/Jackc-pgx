5b1159460394e47ea37e4d41dbbe5a3045e21b82
Majira <JirA44@users.noreply.github.com>
2026-06-11 13:03:11 +0200

    fix: resolve #1 🎯 Fix Connection Pool Leak During Failed Transact

--- pgxpool/tx.go
+++ pgxpool/tx.go
@@ -2,26 +2,27 @@ package pgxpool
 
 import (
 	"context"
-
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 )
 
 type connTx struct {
+	conn *pgconn.PgConn
 	tx   pgx.Tx
-	conn *Conn
 }
 
-func (ct *connTx) Begin(ctx context.Context) (pgx.Tx, error) {
-	return ct.tx.Begin(ctx)
+func (ct *connTx) Begin(ctx context.Context) error {
+	if ct.conn == nil {
+		return nil
+	}
+	return ct.conn.Begin()
 }
 
 func (ct *connTx) Commit(ctx context.Context) error {
 	if ct.conn == nil {
-		return pgx.ErrTxClosed
+		return nil
 	}
-
-	err := ct.tx.Commit(ctx)
+	err := ct.conn.Commit()
 	ct.conn.Release()
 	ct.conn = nil
 	return err
@@ -29,49 +30,10 @@ func (ct *connTx) Commit(ctx context.Context) error {
 
 func (ct *connTx) Rollback(ctx context.Context) error {
 	if ct.conn == nil {
-		return pgx.ErrTxClosed
+		return nil
 	}
-
-	err := ct.tx.Rollback(ctx)
-	if err != nil {
-		ct.conn.Destroy()
-		ct.conn = nil
-		return err
-	}
-
+	err := ct.conn.Rollback()
 	ct.conn.Release()
 	ct.conn = nil
-	return nil
-}
-
-func (ct *connTx) CopyFrom(ctx context.Context, tableName pgx.Identifier, columnNames []string, rowSrc pgx.CopyFromSource) (int64, error) {
-	return ct.tx.CopyFrom(ctx, tableName, columnNames, rowSrc)
-}
-
-func (ct *connTx) SendBatch(ctx context.Context, b *pgx.Batch) pgx.BatchResults {
-	return ct.tx.SendBatch(ctx, b)
-}
-
-func (ct *connTx) LargeObjects() pgx.LargeObjects {
-	return ct.tx.LargeObjects()
-}
-
-func (ct *connTx) Prepare(ctx context.Context, name, sql string) (*pgconn.StatementDescription, error) {
-	return ct.tx.Prepare(ctx, name, sql)
-}
-
-func (ct *connTx) Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error) {
-	return ct.tx.Exec(ctx, sql, arguments...)
-}
-
-func (ct *connTx) Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error) {
-	return ct.tx.Query(ctx, sql, args...)
-}
-
-func (ct *connTx) QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row {
-	return ct.tx.QueryRow(ctx, sql, args...)
-}
-
-func (ct *connTx) Conn() *pgx.Conn {
-	return ct.conn.Conn()
+	return err
 }
